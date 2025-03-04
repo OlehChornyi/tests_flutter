ef0d52b59dff5a586095d295ca17b981cff2d350
oleh <phd.chornyi@gmail.com>
2025-03-04 15:17:16 +0200

    3rd: maths tests (2 more)

--- test/maths_utils_test.dart
+++ test/maths_utils_test.dart
@@ -2,15 +2,41 @@ import 'package:tests_flutter/maths_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Check for two numbers addition', () {
-    //Arrange
-    var a = 10;
-    var b = 10;
+  group('Math utils tests', () {
+    test('Check for two numbers addition', () {
+      //Arrange
+      var a = 10;
+      var b = 10;
 
-    //Act
-    var sum = add(a, b);
+      //Act
+      var sum = add(a, b);
 
-    //Assert
-    expect(sum, 20);
+      //Assert
+      expect(sum, 20);
+    });
+
+    test('Check for two numbers substraction', () {
+      //Arrange
+      var a = 10;
+      var b = 10;
+
+      //Act
+      var subs = substract(a, b);
+
+      //Assert
+      expect(subs, 00);
+    });
+
+    test('Check for two numbers multiplication', () {
+      //Arrange
+      var a = 10;
+      var b = 10;
+
+      //Act
+      var mult = multiply(a, b);
+
+      //Assert
+      expect(mult, 100);
+    });
   });
 }
